8c1be8f878c74be9d5f1f6693bdc3161c664aa03
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-09-24 18:46:06 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118058/latihan15logika/PBOIF210118058Latihan15Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118058.latihan15logika;
+
+/**
+ *
+ * @author user
+ * * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210118058Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println(" a && b = " + (a&&b));
+        System.out.println(" a || b = " + (a||b));
+        System.out.println(" !a = " + !a);
+        System.out.println(" !(a && b) = " + !(a&&b));
+    }
+    
+}
